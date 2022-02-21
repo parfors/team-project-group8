f579b28a7e6dba867d8a08dbbb0d3e4529d6385e
Khivr Vitali <khivr.vitali@gmail.com>
2022-02-22 01:24:49 +0200

    two-btn

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-hero-modal-open]'),
+    openModal2Btn: document.querySelector('[data-hero-mob-modal-open]'),
     closeModalBtn: document.querySelector('[data-hero-modal-close]'),
     modal: document.querySelector('[data-hero-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModal2Btn.addEventListener('click', toggleModal2);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleModal2() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
