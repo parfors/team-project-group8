44ddc8c2ff13c91de72b1a0f570b05377148ca61
Khivr Vitali <khivr.vitali@gmail.com>
2022-02-23 09:39:03 +0200

    fix-script

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,34 +1,18 @@
 (() => {
-  const mobileMenu = document.querySelector('[data-hero-modal]');
-  const openMenuBtn = document.querySelector('[data-hero-modal-open]');
-  const openMenuBtn2 = document.querySelector('[data-hero-mob-modal-open]');
-  const closeMenuBtn = document.querySelector('[data-hero-modal-close]');
-
-
-  const toggleMenu = () => {
-  const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    openMenuBtn2.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-hidden');
-
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  openMenuBtn2.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-hidden');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    openMenuBtn2.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
-
+    const refs = {
+      openHeroModalBtn: document.querySelector('[data-hero-modal-open]'),
+      openHeroModalBtn2: document.querySelector('[data-hero-mob-modal-open]'),
+      closeHeroModalBtn: document.querySelector('[data-hero-modal-close]'),
+      modal: document.querySelector('[data-hero-modal]'),
+    };
+  
+  refs.openHeroModalBtn.addEventListener('click', toggleHeroModal);
+  refs.openHeroModalBtn2.addEventListener('click', toggleHeroModal);
+    refs.closeHeroModalBtn.addEventListener('click', toggleHeroModal);
+   
+      function toggleHeroModal() {
+      refs.modal.classList.toggle('is-hidden');
+      }
+  
+  })();
 
